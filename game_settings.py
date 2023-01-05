70b6066c1b7d2d714888c6dbabf4c598f156f889
Elena <tataushelena@gmail.com>
2023-01-05 21:51:51 +0100

    Created the Settings class (background color + screen dimensions)

---
+++ game_settings.py
@@ -0,0 +1,8 @@
+class GameSettings:
+    """A class to store all setting for the game."""
+
+    def __init__(self):
+        """Initialize the game settings."""
+        self.screen_height = 750
+        self.screen_width = 1200
+        self.background_color = (153, 217, 234)
