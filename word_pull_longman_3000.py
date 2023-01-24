44f8f3998e6027bf9c03d02e31da805d66fe9d6f
Elena <tataushelena@gmail.com>
2023-01-24 15:41:25 +0100

    Changed the pull of words to 3000 most frequent words list (Longman Communication 3000)

---
+++ word_pull_longman_3000.py
@@ -0,0 +1,26 @@
+import json
+
+with open('longman_3000_list.txt', encoding='utf-8') as input_data:
+    frequent_words_list = input_data.readlines()
+
+frequent_words_list = [line.split(' ')[0].replace(',', '') for line in frequent_words_list]
+
+words_with_lenths = {3: [], 4: [], 5: [], 6: [], 7: [], 8:[], 9: [], 10: []}
+
+def filling_in_word_pull(word_length):
+    for word in frequent_words_list:
+        if len(word) == word_length and word.lower() not in words_with_lenths[word_length]:
+            words_with_lenths[word_length].append(word.lower())
+
+filling_in_word_pull(3)
+filling_in_word_pull(4)
+filling_in_word_pull(5)
+filling_in_word_pull(6)
+filling_in_word_pull(7)
+filling_in_word_pull(8)
+filling_in_word_pull(9)
+filling_in_word_pull(10)
+print(words_with_lenths)
+
+with open('word_pull.json', mode='w',encoding='utf-8') as jason_file:
+    json.dump(words_with_lenths, jason_file, indent=2, ensure_ascii=False)
